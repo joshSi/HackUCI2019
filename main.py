aec134127a0c0c94a4cab7fe6fde6f3faf991abd
Rohan Mannem <rohan@dhcp-v102-125.mobile.uci.edu>
2019-02-16 04:46:56 -0800

    Add user location & simple server

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from flask import Flask, render_template, request
 from flask_googlemaps import GoogleMaps
 from flask_googlemaps import Map, icons
 from functools import *
+import geocoder
 
 app = Flask(__name__)
 
 GoogleMaps(app, key="AIzaSyASiSZuUB8z7D3Kd9ZoIIz3Ba4YWabpK1Q")
+
 @app.route("/")
 def test_view():
     clustermap = Map(
@@ -16,22 +18,27 @@ def test_view():
         style="height: 425px; width: 1000px;",
         markers=[
             {
+                'icon' : 'http://maps.google.com/mapfiles/ms/icons/red-dot.png',
                 'lat': 37.4500,
                 'lng': -122.1350
             },
             {
+                'icon' : 'http://maps.google.com/mapfiles/ms/icons/red-dot.png',
                 'lat': 37.4400,
                 'lng': -122.1350
             },
             {
+                'icon' : 'http://maps.google.com/mapfiles/ms/icons/red-dot.png',
                 'lat': 37.4300,
                 'lng': -122.1350
             },
             {
+                'icon' : 'http://maps.google.com/mapfiles/ms/icons/red-dot.png',
                 'lat': 36.4200,
                 'lng': -122.1350
             },
             {
+                'icon' : 'http://maps.google.com/mapfiles/ms/icons/red-dot.png',
                 'lat': 36.4100,
                 'lng': -121.1350
             },
@@ -56,7 +63,13 @@ def test_view():
             'lng' : (reduce((lambda x, y: x + y), lngList[1:]) / len(lngList[1:]))}
     clustermap.markers.append(center.copy())
 
+    myloc = geocoder.ip('me')
+    meMarker = {'icon' : 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png', 
+            'lat' : myloc.lat,
+            'lng' : myloc.lng}
+    clustermap.markers.append(meMarker)
+
     return render_template('home.html', clustermap=clustermap)
 
 if __name__ == "__main__":
-    app.run(debug=True, use_reloader=True)
+    app.run(debug=True, use_reloader=True, host = "0.0.0.0", port = 80)
